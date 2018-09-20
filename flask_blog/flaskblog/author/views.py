a37a6a9e2fae205e0d66b2564f37ee6e21d59a01
Jinjin Liang <jliang0718@outlook.com>
2018-09-20 15:01:56 -0700

    add macro
    define a function in macro to decrease the repeating work

--- flask_blog/flaskblog/author/views.py
+++ flask_blog/flaskblog/author/views.py
@@ -1,5 +1,5 @@
 from flaskblog import app
-from flask import  render_template, redirect
+from flask import  render_template, redirect, url_for
 from author.form import RegisterForm
 
 @app.route('/login')
@@ -11,4 +11,11 @@ def login():
 
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
     return render_template('author/register.html', form=form)
+
+@app.route('/success')
+
+def success():
+    return "Author registered!"
