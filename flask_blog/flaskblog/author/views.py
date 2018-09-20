e78ba4dcb7545412df4345ac0592f67ddb23c72f
Jinjin Liang <jliang0718@outlook.com>
2018-09-20 14:15:13 -0700

    add registration form

--- flask_blog/flaskblog/author/views.py
+++ flask_blog/flaskblog/author/views.py
@@ -1,6 +1,14 @@
 from flaskblog import app
+from flask import  render_template, redirect
+from author.form import RegisterForm
 
 @app.route('/login')
 
 def login():
     return "Hello, User!"
+
+@app.route('/register', methods=('GET', 'POST'))
+
+def register():
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
