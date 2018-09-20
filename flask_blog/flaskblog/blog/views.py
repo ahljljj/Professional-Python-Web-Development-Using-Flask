97a7dfae192cba83c0ffd275e16a4b49aef12568
Jinjin Liang <jliang0718@outlook.com>
2018-09-20 15:50:32 -0700

    add "create blog" model

--- flask_blog/flaskblog/blog/views.py
+++ flask_blog/flaskblog/blog/views.py
@@ -1,9 +1,31 @@
 from flaskblog import app
+from flask import render_template, redirect, flash, url_for
+from blog.form import SetupForm
+from flaskblog import db
+from author.models import Author
+from blog.models import Blog
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     return "Hello World!"
+
+@app.route('/admin')
+
+def admin():
+    blogs = Blog.query.count()
+    if blogs == 0:
+        return redirect(url_for('setup'))
+    return render_template('blog/admin.html')
+
+@app.route('/setup')
+
+def setup():
+    form = SetupForm()
+
+
+
+    return render_template('blog/setup.html', form=form)
 
 
