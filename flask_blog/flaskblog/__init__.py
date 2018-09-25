0d50af53fbd315bf91445cd6a5b40b9ee11db092
Jinjin Liang <jliang0718@outlook.com>
2018-09-25 15:53:33 -0700

    add image folder and upgrade database

--- flask_blog/flaskblog/__init__.py
+++ flask_blog/flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flaskext.markdown import Markdown
+from flask_uploads import  UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -12,7 +13,13 @@ migrate = Migrate(app, db)
 
 
 #Markdown
-Markdown(app)
+md = Markdown(app, extensions=['fenced_code','tables'])
+
+#images
+
+uploaded_images = UploadSet('images', IMAGES)
+configure_uploads(app, uploaded_images)
+
 
 from blog import views
 from author import views
--- flask_blog/flaskblog/settings.py
+++ flask_blog/flaskblog/settings.py
@@ -12,3 +12,5 @@ DB_HOST = os.getenv('IP', 'localhost')
 DB_URL = 'mysql+pymysql://%s:%s@%s/%s' %(DB_USERNAME, DB_PASSWORD, DB_HOST, BLOG_DATABASE_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+UPLOADED_IMAGES_DEST = 'H:/udemy/flask/flask_blog/flaskblog/static/images'
+UPLOADED_IMAGES_URL = '/static/images'
